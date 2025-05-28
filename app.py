5c15465190103844e98b81dfadf13bcddf9b2ee6
Eko Prabowo <57608542+namaekoya@users.noreply.github.com>
2025-05-28 23:10:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 
 # Load model yang sudah dilatih
 @st.cache_data()
-def load_model(model.pkl):
+def load_model(model_path):
     with open(model.pkl, 'rb') as model_file:
         model = pickle.load(model_file)
     return model
